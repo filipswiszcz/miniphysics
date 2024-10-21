322d15b06875f0332c4a621941c07284126b19f3
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2024-10-21 02:52:46 +0200

    window

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,50 @@
 
 #define GL_SILENCE_DEPRECATION
 #include <GLFW/glfw3.h>
-
 #endif
 
 #include <iostream>
 
-int main(int argc, char **argv) {
+const size_t WIDTH = 1280;
+const size_t HEIGHT = 960;
+const char* WINDOW_NAME = "rush";
+
+void input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    GLFWwindow *window;
+
     if (!glfwInit()) return -1;
-    std::cout << "hi" << std::endl;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, WINDOW_NAME, NULL, NULL);
+    if (!window) {
+        std::cout << "failed" << std::endl;
+        glfwTerminate(); return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // inputs
+        input(window);
+
+        // render commands
+
+        // idk something
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
