4551a0a1f2269ead03f55fec0a35da1e3099ed80
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-01-09 23:30:31 +0100

    lazy obj loader

--- include/rush/util/resource_loader.hpp
+++ include/rush/util/resource_loader.hpp
@@ -1,5 +1,15 @@
+#ifndef RESOURCE_LOADER_H
+#define RESOURCE_LOADER_H
+
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <regex>
+#include <vector>
+
+extern "C" {
+    #include <rush/util/log.h>
+}
 
 #ifdef __APPLE__
     #define GL_SILENCE_DEPRECATION
@@ -7,18 +17,15 @@
     #include <GLFW/glfw3.h>
 #endif
 
-extern "C" {
-    #include <rush/util/log.h>
-}
-
-#ifndef RESOURCE_LOADER_H
-#define RESOURCE_LOADER_H
+#include <glm/glm.hpp>
 
 namespace util {
 
     // unsigned char *read_img_file(const char *filename);
 
     unsigned int temp_load_texture(const std::string& name);
+
+    void load_obj(const std::string& name, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &uvs, std::vector<glm::vec3> &normals);
 
 } // namespace util
 
--- src/util/resource_loader.cpp
+++ src/util/resource_loader.cpp
@@ -32,4 +32,74 @@ unsigned int util::temp_load_texture(const std::string& filename) {
     return temp_texture;
 }
 
+void util::load_obj(const std::string& name, std::vector<glm::vec3> &vertices, std::vector<glm::vec2> &uvs, std::vector<glm::vec3> &normals) {
+    std::vector<glm::vec3> temp_vertices;
+    std::vector<glm::vec2> temp_uvs;
+    std::vector<glm::vec3> temp_normals;
+    std::vector<unsigned int> vertex_indices, uv_indices, normal_indices;
+
+    std::ifstream file(name);
+    if (!file) {
+        std::cout << "file not found" << std::endl; return;
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.starts_with("v ")) {
+            glm::vec3 vertex;
+            sscanf(line.c_str(), "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+            temp_vertices.push_back(vertex);
+        } else if (line.starts_with("vt")) {
+            glm::vec2 uv;
+            sscanf(line.c_str(), "vt %f %f", &uv.x, &uv.y);
+            temp_uvs.push_back(uv);
+        } else if (line.starts_with("vn")) {
+            glm::vec3 normal;
+            sscanf(line.c_str(), "vn %f %f %f", &normal.x, &normal.y, &normal.z);
+            temp_normals.push_back(normal);
+        } else if (line.starts_with("f")) {
+            std::string vertex_a, vertex_b, vertex_c;
+            unsigned int vertex_id[3], uv_id[3], normal_id[3];
+            std::regex pattern("([a-z]) (\\d+)/(\\d+)/(\\d+) (\\d+)/(\\d+)/(\\d+) (\\d+)/(\\d+)/(\\d+)");
+            std::smatch match;
+            if (std::regex_match(line, match, pattern)) {
+                vertex_id[0] = std::stoi(match[2].str());
+                uv_id[0] = std::stoi(match[3].str());
+                normal_id[0] = std::stoi(match[4].str());
+                vertex_id[1] = std::stoi(match[5].str());
+                uv_id[1] = std::stoi(match[6].str());
+                normal_id[1] = std::stoi(match[7].str());
+                vertex_id[2] = std::stoi(match[8].str());
+                uv_id[2] = std::stoi(match[9].str());
+                normal_id[2] = std::stoi(match[10].str());
+            }
+            vertex_indices.push_back(vertex_id[0]);
+            vertex_indices.push_back(vertex_id[1]);
+            vertex_indices.push_back(vertex_id[2]);
+            uv_indices.push_back(uv_id[0]);
+            uv_indices.push_back(uv_id[1]);
+            uv_indices.push_back(uv_id[2]);
+            normal_indices.push_back(normal_id[0]);
+            normal_indices.push_back(normal_id[1]);
+            normal_indices.push_back(normal_id[2]);
+        }
+    }
+
+    file.close();
+
+    for (unsigned int i = 0; i < vertex_indices.size(); i++) {
+        unsigned int vertex_id = vertex_indices[i];
+        unsigned int uv_id = uv_indices[i];
+        unsigned int normal_id = normal_indices[i];
+
+        glm::vec3 vertex = temp_vertices[vertex_id - 1];
+        glm::vec2 uv = temp_uvs[uv_id - 1];
+        glm::vec3 normal = temp_normals[normal_id - 1];
+
+        vertices.push_back(vertex);
+        uvs.push_back(uv);
+        normals.push_back(normal);
+    }
+}
+
 #pragma endregion Resource_Loader }
