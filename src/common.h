c5caffa3a9f2a44d21209dabb4f865c23d28aa88
filipswiszcz <65617364+filipswiszcz@users.noreply.github.com>
2025-08-12 19:56:09 +0200

    add starts_with() for std::string

--- src/common.h
+++ src/common.h
@@ -4,7 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string>
+
 #define ASSERT(_e, ...) if (!(_e)) {fprintf(stderr, __VA_ARGS__);}
 #define QSSERT(_e, ...) if (!(_e)) {fprintf(stderr, __VA_ARGS__); exit(1);}
+
+// unsafe (it __does_not__ check if view is longer than prefix)
+static int starts_with(const std::string &text, const std::string &prefix) {
+    return text.compare(0, prefix.size(), prefix) == 0;
+}
 
 #endif // !__COMMON_H__
